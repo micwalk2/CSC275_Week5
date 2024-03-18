e4ddf78a95985d44cf41b1fdfd30e4049da7172f
Michael Walker <micwalk2@uat.edu>
2024-03-18 13:51:23 -0700

    Set up project and comment objectives.

---
+++ Final/Main.cpp
@@ -0,0 +1,14 @@
+// Michael Walker
+// CSC275
+// Week 5: Final Project
+
+// The application should not be something just to demonstrate the requirements, it should "do" something.
+// The application should have a clear theme.
+// The application is required to have a good User Experience (UX).
+// The application is required to be over commented.
+// The application uses multiple files.
+// The application MUST include a minimum of (smart pointers or multithreading) AND OO (3 classes - 1 base class and 2 derived classes).
+
+// 
+
+#include <iostream>
